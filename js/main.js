2e5ff2a9ad731b775b9245dfb23f4b32df5e32a5
Jake Enzminger <enzmi003@ad.umn.edu>
2018-11-08 10:45:06 -0600

    latlng shows up but is undefined

--- js/main.js
+++ js/main.js
@@ -30,7 +30,8 @@ info.update = function (props) {
     (props ? "<h4>Precinct: "+props.PCTNAME+"</h4>"+
 	"<table><tr><td>Democratic votes: </td><td>"+props.USPRSDFL+"</td></tr>"+
 	"<tr><td>Republican votes: </td><td>"+props.USPRSR+"</td></tr>"+
-	"<tr><td>Total votes: </td><td>"+props.USPRSTOTAL+"</td></tr></table>" : "Hover over a precinct to see vote counts");
+	"<tr><td>Total votes: </td><td>"+props.USPRSTOTAL+"</td></tr>"+
+    "<tr><td>latitude and longitude: </td><td>"+props.LATLNG+"</td></tr>"+"</table>" : "Hover over a precinct to see vote counts");
 };
 
 info.addTo(map);
@@ -168,7 +169,7 @@ request.then(function(values){
         console.log('mouse lat/long', eventObject.latlng)
     // Update mouse cordinites with HTML element with event latlng
         
-        document.getElementById("mouseCoordinatesBox").innerHTML = "newtext";
+        document.getElementById("mouseCoordinatesBox").innerHTML = "latlng";
         
     }
   	//  - second, register an event listener with the map
@@ -178,5 +179,6 @@ request.then(function(values){
   	//  - when it doubt use Google
   	//    (something to the effect of "leaflet display mouse coordinates"
   	//    should help)
+    
 
 });
